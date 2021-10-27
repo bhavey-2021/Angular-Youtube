785a29da7933e5efe0a464134fe9597f0c8cedb7
bhavey malhotra <bhavey.m@gmail.com>
2021-10-27 08:34:06 +0530

    Two way data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DatabindComponent } from './databind/databind.component';
 import { ClassStyleComponent } from './databind/class-style/class-style.component';
 import { EventbindComponent } from './databind/eventbind/eventbind.component';
+import { TwoWayComponent } from './databind/two-way/two-way.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DatabindComponent,
     ClassStyleComponent,
-    EventbindComponent
+    EventbindComponent,
+    TwoWayComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
